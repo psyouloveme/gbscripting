12a60af51de50e8b0f40c17dbc411a37f06e53a0
psyouloveme <4607641+psyouloveme@users.noreply.github.com>
2019-10-03 22:23:02 -0700

    Check sprite size, draw correct size overlays

--- overlay_oam_sprites.lua
+++ overlay_oam_sprites.lua
@@ -1,16 +1,20 @@
 local OAMOffset = 0xFE00
+local LCDCRegisterOffset = 0xFF40
 
-function drawSpriteLocation(x, y, spriteNumber, color)
+local objSizeFlag = 4
+
+--- Draw a sprite overlay with slot number
+function drawSpriteLocation(x, y, spriteNumber, color, spriteHeight)
     if (x > 0 and y > 0 and x < 168 and y < 160) then
-        if (spriteNumber < 10) then
-            spriteNumber = "0"..spriteNumber
-        end
         gui.drawText(x - 10, y - 16, spriteNumber, "red", 0x00000000, 8, nil, nil, "left", "top")
-        -- gui.drawPixel(spriteX, spriteY, "blue")
-        gui.drawRectangle(x - 8, y - 16, 7, 15, 0x550000FF, 0x550000FF)
+        gui.drawRectangle(x - 8, y - 16, 7, spriteHeight - 1, 0x550000FF, 0x550000FF)
+        -- gui.drawPixel(x-8, y-16, "red")
+        -- gui.drawPixel(x-1, y - 16, "green")
     end
 end
 
+--- Get OAM Attributes for a sprite slot by index
+--- Reads 4 bytes from 0xFE00 + 4 * sprite slot
 function getOAMAttrs(slot) 
     if (slot < 40 and slot >= 0) then
         local spriteOffset = OAMOffset + (slot * 4)
@@ -22,15 +26,30 @@ function getOAMAttrs(slot)
     end
 end
 
-function drawOAMSprites()
+--- Draw an overlay on all of the 40 sprites stored in OAM
+function drawOAMSprites(spriteHeight)
     local color = 0x550000FF
     for i = 0, 39 do
         local y, x, tile, flags = getOAMAttrs(i)
-        drawSpriteLocation(x, y, i, color)
+        drawSpriteLocation(x, y, i, color, spriteHeight)
     end
 end
 
+
+--- Check the LCD Control register to see what the sprite height is set to.
+--- Sprites are always 8x8 or 8x16.
+--- $FF40 - 1 byte
+function getLCDCSpriteHeight()
+    local lcdcRegVal = memory.readbyte(LCDCRegisterOffset)
+    -- print(bizstring.binary(lcdcRegVal))
+    if (bit.check(lcdcRegVal, 2)) then
+        return 16
+    end
+    return 8
+end
+
 while true do
-    drawOAMSprites()
+    local spriteHeight = getLCDCSpriteHeight()
+    drawOAMSprites(spriteHeight)
     emu.frameadvance()
 end
